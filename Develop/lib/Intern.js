4305ff8ad008d4fe1a439de2654cd40fc205e5c3
Christina Flores <cdflori1@gmail.com>
2020-10-20 13:05:27 -0700

    Added in code for Employee, Intern, Manager, Engineer.js files.

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+    constructor(name, id, email) {
+    super(name, id, email)
+    this.school = this.school;
+}
+
+getRole(){
+    return "Intern";
+}
+
+getSchool(){
+    return this.school;
+}
+
+}
+
+module.exports = Intern
